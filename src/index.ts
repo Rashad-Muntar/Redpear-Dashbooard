e8b5b03091bf7680b89e71237df2e144aca9adf2
Rashad Muntar <muntartoure@gmail.com>
2024-04-04 23:59:49 +0000

    update home page

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express, { Express } from 'express';
 import path from 'path';
 import dotenv from 'dotenv';
+import { getPolicy } from './controllers/policy';
 import router from './routes';
 
 dotenv.config();
@@ -13,14 +14,19 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", 'ejs'); 
 app.use(express.static("public"));
 
-app.use('/', router);
 
-(async () => {
-  try {
-    app.listen(port, () => {
-      console.log(`⚡️[server]: App is running at http://localhost:${port}`);
-    });
-  } catch (error: any) {
-    return error.message;
-  }
-})();
+// app.use('/', router);
+app.get("/", getPolicy)
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: App is running at http://localhost:${port}`);
+});
+// (async () => {
+//   try {
+//     app.listen(port, () => {
+//       console.log(`⚡️[server]: App is running at http://localhost:${port}`);
+//     });
+//   } catch (error: any) {
+//     return error.message;
+//   }
+// })();
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,9 @@
 import express from "express";
-import { Request, Response } from 'express'
 import { getPolicies, getPolicy } from "../controllers/policy";
 const router = express.Router();
 
 
-router.get("/", (req: Request, res: Response) => {
-    getPolicies(req, res)
-});
-
+router.get("/", () =>  getPolicies);
 router.get("/policy/:id", getPolicy);
 
 export default router;
