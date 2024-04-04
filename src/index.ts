ec9ff8ee5ff73ca8c94dbbdf500cc729b46d7d57
Rashad Muntar <muntartoure@gmail.com>
2024-04-04 23:14:11 +0000

    debug deploy fail

--- src/index.ts
+++ src/index.ts
@@ -9,9 +9,10 @@ const app: Express = express();
 const port: number = Number(process.env.PORT) || 3000;
 
 // Serve static files from the public directory
-app.use(express.static("public"));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", 'ejs'); 
+app.use(express.static("public"));
+
 
 // app.use('/', router);
 app.get("/", (req, res) => {
