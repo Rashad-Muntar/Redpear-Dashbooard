7c4c1bca5ce11d6e900a8c73c7eaaa35581da1f9
Rashad Muntar <muntartoure@gmail.com>
2024-04-05 00:31:13 +0000

    server error debugging

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,9 @@
 import express from "express";
-import { Request, Response } from 'express'
 import { getPolicies, getPolicy } from "../controllers/policy";
 const router = express.Router();
 
 
-router.get("/", () =>  (req: Request, res: Response) => {
-    res.send("HOME PAGE")
-});
+router.get("/",  getPolicies);
 router.get("/policy/:id", getPolicy);
 
 export default router;
