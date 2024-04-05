c58631b7c1d7933ba392c7e2f514d7db1333aece
Rashad Muntar <muntartoure@gmail.com>
2024-04-05 03:04:19 +0000

    resolve casing

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,6 @@ import express from "express";
 import { getPolicies, getPolicy } from "../controllers/policy";
 const router = express.Router();
 
-
 router.get("/",  getPolicies);
 router.get("/policy/:id", getPolicy);
 
