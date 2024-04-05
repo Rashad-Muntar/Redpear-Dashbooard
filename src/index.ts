c20ad7b833bd4bf0ebdb7f633f1a851cda5b5821
Rashad Muntar <muntartoure@gmail.com>
2024-04-05 08:30:06 +0000

    update dashboard UI

--- src/index.ts
+++ src/index.ts
@@ -9,15 +9,12 @@ dotenv.config();
 const app: Express = express();
 const port: number = Number(process.env.PORT) || 3000;
 
-// Serve static files from the public directory
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", 'ejs'); 
 app.use(express.static("public"));
 
 
 app.use('/', router);
-// app.get("/", getPolicy)
-
 
 (async () => {
   try {
